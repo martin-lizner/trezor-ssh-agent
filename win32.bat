eeb66d71f449de424760d5e3041ca93829425bae
Gary Rowe <g.rowe@froot.co.uk>
2014-09-22 15:35:24 +0100

    Ensure install

--- win32.bat
+++ win32.bat
@@ -1,6 +1,6 @@
 @echo off
 git pull
-mvn clean compile
+mvn clean install
 cd examples
 mvn clean compile exec:java -Dexec.mainClass="org.multibit.hd.hardware.examples.trezor.usb.UsbMonitoringExample"
 cd ..
