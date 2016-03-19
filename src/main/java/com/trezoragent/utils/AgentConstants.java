3d7edbeff100bb65ee5ad2a2fe2ccbe11ee7c152
Martin Lizner <martin.lizner@gmail.com>
2016-03-19 22:11:03 +0100

    prepare version info for next release

--- src/main/java/com/trezoragent/utils/AgentConstants.java
+++ src/main/java/com/trezoragent/utils/AgentConstants.java
@@ -52,7 +52,7 @@ public final class AgentConstants {
     public static final String LOG_FILE_NAME = "Trezor_Agent.log"; // + change path in logger.properties
 
     public static String MUTEX_NAME = "Trezor_Agent_Mutex"; // mutex for installer - correlates with instaler.iss
-    public static final String VERSION = "0.9.0"; // global version, pom.xml
+    public static final String VERSION = "1.0.0"; // global version, pom.xml
 
     /*
      * Error messages keys
