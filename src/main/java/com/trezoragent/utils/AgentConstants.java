831642c0fb68e620ce198b3e9968793bacb8ce30
Martin Lizner <martin.lizner@gmail.com>
2016-05-04 11:56:55 +0200

    bump version to 1.02

--- src/main/java/com/trezoragent/utils/AgentConstants.java
+++ src/main/java/com/trezoragent/utils/AgentConstants.java
@@ -67,7 +67,7 @@ public final class AgentConstants {
     public static final String SETTINGS_FILE_NAME = "Trezor_Agent.properties";
 
     public static String MUTEX_NAME = "Trezor_Agent_Mutex"; // mutex for installer - correlates with instaler.iss
-    public static final String VERSION = "1.0.1"; // global version, pom.xml
+    public static final String VERSION = "1.0.2"; // global version, pom.xml
 
     /*
      * Error messages keys
