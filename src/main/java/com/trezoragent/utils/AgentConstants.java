20d3910c8f661aac174fe13ab8e8302af646ed8d
Martin Lizner <martin.lizner@gmail.com>
2016-12-17 10:28:17 +0100

    drop jdk7 support

--- src/main/java/com/trezoragent/utils/AgentConstants.java
+++ src/main/java/com/trezoragent/utils/AgentConstants.java
@@ -67,7 +67,7 @@ public final class AgentConstants {
     public static final String SETTINGS_FILE_NAME = "Trezor_Agent.properties";
 
     public static String MUTEX_NAME = "Trezor_Agent_Mutex"; // mutex for installer - correlates with instaler.iss
-    public static final String VERSION = "1.0.3"; // global version, pom.xml
+    public static final String VERSION = "1.0.4-SNAPSHOT"; // global version, pom.xml
 
     /*
      * Error messages keys
